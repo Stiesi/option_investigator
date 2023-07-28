bfb1001f65fc8efd4e2f50627ec23e3278ffcc66
Jens J <stiesel_diesel@yahoo.de>
2023-07-28 18:42:04 +0200

    watchlist manager simple

--- pages/3_list_manager.py
+++ pages/3_list_manager.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from pydantic import BaseModel,Field
 import pandas as pd
 from deta import Deta
 import json
@@ -18,38 +19,61 @@ def read_base():
 
 # read watchlists of shares
 #
+class Watchlist(BaseModel):
+    key : str
+    name: str = Field()
+    symbols: list[str] = Field(unique_items=True)
 
-def read_all_wls():
+def wls_read_all() -> list[Watchlist]:
     db_content = db_wlshare.fetch().items
     return db_content
+    
+def wls_read(key:str) -> Watchlist:
+    #wls = db_wlshare.fetch({"name":name}).items[0]
+    wls = db_wlshare.get(key)
+    return Watchlist(key=wls['key'],name=wls['name'],symbols=wls['symbols'])
+    
+def wls_put(wls:Watchlist):
+    return db_wlshare.insert(dict(name=wls.name,symbols=wls.symbols))
 
-def read_one_wls(name):
-    return db_wlshare.fetch(name=name)
+def wls_update(wls:Watchlist):
+    return db_wlshare.update(dict(name=wls.name,symbols=wls.symbols),wls.key)
 
-def create_wls(name='',listofshares=[]):
-    data = dict(name=name,listofshares=listofshares)
-    try:
-        db_wlshare.put(data)
-    except:
-        print('Error in creating watchlist')
+def wls_delete(key:str):    
+    return db_wlshare.delete(key)
 
-def update_wls_list(name,listofshares=[]):
-    #data = db_wlshare.fetch().items
-    try:
-        data = db_wlshare.fetch(name=name)
-        data['listofshares']=listofshares
-        db_wlshare.update(data)
-    except:
-        print('error updating %s'%name)
 
-def update_wls_name(name,newname):
-    data = db_wlshare.fetch(name=name)
-    data['name']=newname
-    db_wlshare.fetch(name=name)
+# def read_all_wls():
+#     db_content = db_wlshare.fetch().items
+#     return db_content
 
-def delete_wls_name(name):
-    data = db_wlshare.fetch(name=name)
-    db_wlshare.delete(data['key'])
+# def read_one_wls(name):
+#     return db_wlshare.fetch(name=name)
+
+# def create_wls(name='',listofshares=[]):
+#     data = dict(name=name,listofshares=listofshares)
+#     try:
+#         db_wlshare.put(data)
+#     except:
+#         print('Error in creating watchlist')
+
+# def update_wls_list(name,listofshares=[]):
+#     #data = db_wlshare.fetch().items
+#     try:
+#         data = db_wlshare.fetch(name=name)
+#         data['listofshares']=listofshares
+#         db_wlshare.update(data)
+#     except:
+#         print('error updating %s'%name)
+
+# def update_wls_name(name,newname):
+#     data = db_wlshare.fetch(name=name)
+#     data['name']=newname
+#     db_wlshare.fetch(name=name)
+
+# def delete_wls_name(name):
+#     data = db_wlshare.fetch(name=name)
+#     db_wlshare.delete(data['key'])
 
 
 def dataframe_with_selections(df,preselect=[]):
@@ -72,51 +96,74 @@ def dataframe_with_selections(df,preselect=[]):
     return selected_rows.drop('Select', axis=1)
 
 db_df = read_base()
-st.dataframe(db_df)
+#st.dataframe(db_df)
 
-tab1,tab2,tab3 = st.tabs(['Watchlists','Share Selector','Option Selector'])
-with tab1:
-    col1,col2,col3 = st.columns((1,1,1))
-    with col1:
-        new = st.button('New')
-    watchlists = db_wlshare.fetch().items
-    watchlists_names = [watchlist['name'] for watchlist in watchlists]
-    if watchlists:  # watchlists exist
-        name_selected = st.selectbox('Watchlist',watchlists_names)
-        wls_selected = db_wlshare.fetch(dict(name=name_selected)).items[0]
-        list_of_tickers = json.loads(wls_selected['watchlist_shares'])['symbol_ticker'].values()
+all_lists = wls_read_all()
+listofwatchlists = [wls['name'] for wls in all_lists]
 
-        #selected = list_of_tickers
-    else: 
-        name_selected = 'Watchlist 1'
-        list_of_tickers = []
+mylist = st.sidebar.selectbox('Watchlist',options=listofwatchlists)
+# dict of selected name
+data = all_lists[listofwatchlists.index(mylist)]
+mywls = Watchlist(key=data['key'],name=data['name'],symbols=data['symbols'])
+mykey = mywls.key
+
+
+menu = ['New','View','Update','Delete']
+select = st.sidebar.selectbox('Action',options=menu,index=1)
+
+
+if select =='New':
+    newcount = len(listofwatchlists)+1
+    name = st.text_input("Watch List Name",value='Watchlist %d'%newcount)
     
-    ws_create = st.text_input('New Watchlist',value='Watchlist 1')
-    
 
-with tab2:
-    st.write(name_selected)
-    selection = dataframe_with_selections(db_df[['name','indices','symbol_ticker']],list_of_tickers)
+    selection = dataframe_with_selections(db_df[['name','indices','symbol_ticker']],[])
     st.write("Your selection:")
-    st.write(db_df.loc[selection.index])
-
-    # Create a new database "example-db"
-    # If you need a new database, just use another name.
-
-
-
-    with st.form("form"):
-        name = st.text_input("Watch List Name",value=name_selected)
-        
-        create = st.form_submit_button("Store in database")
-        update = st.form_submit_button("Update in database")
-
+    #st.write(db_df.loc[selection.index])
+    st.dataframe(selection)
+    create = st.button("Save")
     if create:
-        listofshares = db_df[['symbol_ticker','isin']].loc[selection.index].to_json()
-        db_wlshare.put({"name": name, "watchlist_shares": listofshares})
-    if update:
-        data = db_wlshare.fetch(name=name)
-        db_wlshare.update({"name": name, "watchlist_shares": db_df[['symbol_ticker','isin']].loc[selection.index]})
+        # create list of symbols
+        listofsymbols = list(selection['symbol_ticker'].values)
+        mywls = Watchlist(key=name,name=name,symbols=listofsymbols)
+        ok = wls_put(mywls)
+        if ok:
+            st.success('%s saved'%name)
+            mywls = Watchlist(key=ok['key'],name=ok['name'],symbols=ok['symbols'])
+            mykey = mywls.key
 
-with tab3:
-    st.write('Hi')
+if select=='Update':
+    mywls = wls_read(mykey)
+    st.header(mywls.name)
+    selection = dataframe_with_selections(db_df[['name','indices','symbol_ticker']],mywls.symbols)
+    st.dataframe(selection)
+    #st.dataframe(db_df.loc[db_df['symbol_ticker'].isin(mywls.symbols)])
+    update = st.button('Update')
+    if update:
+        listofsymbols = list(selection['symbol_ticker'].values)
+        mywls = Watchlist(key=mywls.key,name=mywls.name,symbols=listofsymbols)
+        ok = wls_update(mywls)
+        if ok:
+            st.success('%s updated'%mywls.name)
+        select='View'
+
+if select=='Delete':
+    #mywls = wls_read(mykey)
+    #confirm = st.button('Delete %s'% mywls.name )
+    st.header(mywls.name)
+    confirm = st.button('Delete List %s'%mywls.name)
+    if confirm:
+        ok = wls_delete(mykey)
+        if ok:
+            st.success('%s deleted'%mywls.name)
+        select='View'
+
+if select =='View':
+    st.header(mywls.name)
+    mywls = wls_read(mykey)
+    #st.write(mywls)
+    #selection = dataframe_with_selections(db_df[['name','indices','symbol_ticker']],mywls['symbols'])
+    st.dataframe(db_df.loc[db_df['symbol_ticker'].isin(mywls.symbols)])
+    
+    
+
